743caec3efef8eac3f0fb523527ed2de0ceb06c2
Mojzi <Mojzi@users.noreply.github.com>
2018-06-07 00:23:54 +0200

    Not added files to commit...

---
+++ Deserted/Assets/Scriptable Objects/HealthPotion.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Potion", menuName = "Potion")]
+public class HealthPotion : Item {
+
+	public int healingPower = 50;
+
+	public override void Use(GameObject user) {
+		user.GetComponent<MovementController>().ChangeHealth(healingPower);
+		Inventory.instance.Remove(this);
+	}
+}
