ae4264ccd0955d9730817e8fe1d12437fa8854d5
Mojzi <Mojzi@users.noreply.github.com>
2018-06-06 09:29:50 +0200

    Added new map, added map transitions

---
+++ Deserted/Assets/Scripts/TeleportScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject destination;
+	public Camera camera;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag.Equals("Player")) {
+			other.GetComponent<Transform>().position = destination.GetComponent<Transform>().position;
+			camera.GetComponent<Transform>().position = destination.GetComponent<Transform>().position;
+		}
+	}
+}
